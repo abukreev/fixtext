894da1be67b15ded81824ad4b1383ac4a6c2ca62
Aleksandr Bukreev <abukreev@gmail.com>
2021-12-23 21:16:19 +0300

    Add -l|--line arg

--- fixtext.sh
+++ fixtext.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
-
-# (c) https://habr.com/ru/post/279499/
-
 IFS="\n"
 
-LOG="/home/abukreev/fixtext.log"
-printf "\n" >> $LOG
+#LOG=$HOME"/fixtext.log"
+LOG="/dev/null"
+echo "------------" >> "$LOG"
 
 BUFFER="$(xsel)"
-printf "BUFFER = \"%s\"\n" $BUFFER >> $LOG
+printf "BUFFER = \"%s\"\n" $BUFFER >> "$LOG"
 
-if [ "$BUFFER" = "" ]; then
-    printf "Buffer is empty" >> $LOG
+if [ -z "$BUFFER" ] && [ $# -gt 0 ]; then
+    printf "111\n" >> "$LOG"
+    if [ "$1" == "-l" ] || [ "$1" == "--line" ]; then
+        printf "222\n" >> "$LOG"
+#        xdotool keyup Ctrl
+        xdotool keyup Pause
+        xdotool key Shift+Left
+        BUFFER="$(xsel)"
+        printf "BUFFER = \"%s\"\n" $BUFFER >> "$LOG"
+#        exit 0
+#        sleep 0.1
+    else
+        printf "333\n" >> "$LOG"
+        printf "Usage: fixtext [-l|--line]\n" >> /dev/stderr
+        exit 1
+    fi
+fi
+
+
+if [ -z "$BUFFER" ]; then
+    printf "Buffer is empty\n" >> "$LOG"
     exit 1
 fi
 
 BACKUP="$(xsel -b)"
+printf "BACKUP = \"%s\"\n" $BACKUP >> "$LOG"
 
 FIXED=$(echo "$BUFFER" | sed "y/abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]{};':\",.\/<>?@#\$^&\`~фисвуапршолдьтщзйкыегмцчняФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯхъХЪжэЖЭбюБЮ№ёЁ/фисвуапршолдьтщзйкыегмцчняФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯхъХЪжэЖЭбю.БЮ,\"№;:?ёЁabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[]{};':\",.<>#\`~/")
-printf "FIXED = \"%s\"\n" $FIXED >> $LOG
+printf "FIXED = \"%s\"\n" $FIXED >> "$LOG"
 
-printf "%s" "$FIXED" | xsel -b -i
+printf "%s" "$FIXED" | xsel -bi
 
-sleep 0.05
+sleep 0.1
 
 xdotool key ctrl+v
+
+#sleep 0.1
+
 xdotool key alt+shift
 
-printf "%s" "$BACKUP" | xsel -b -i
+#sleep 0.1
+
+printf "%s" "$BACKUP" | xsel -bi
+
+xsel -c
 
